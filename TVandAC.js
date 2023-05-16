d712e782957ca47683fa1b3f8c0b26ed9fdeca1c
sikandraomprasad <89696118+sikandraomprasad@users.noreply.github.com>
2023-05-16 15:20:04 +0530

    Add files via upload

---
+++ TVandAC.js
@@ -0,0 +1,54 @@
+Status = "";
+TVandAc_image = "";
+objects=[];
+
+function preload(){
+    TVandAc_image = loadImage("TV and Ac.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+    console.log(results);
+    }
+}
+
+function draw(){
+    image(TVandAc_image,0,0,640,350);
+     
+    if (Status != "")
+    {
+        object_Detector.detect(TVandAc_image,gotResults);
+        r=Math.random(255);
+        g=Math.random(255);
+        b=Math.random(255);
+        for(i=0; i< objects.length;i++)
+        {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("nof").innerHTML = "There are 3 big objects in the image from which cocossd model has detected  " + objects.length + " objects.";
+            fill(r,g,b);
+            stroke(r,g,b);    
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x, objects[i].y ); 
+            noFill();
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height );
+           
+        }
+    }
+}
